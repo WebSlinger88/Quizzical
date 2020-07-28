2796b186594f3a7bd0e2875546df3e0dd363f095
Lewis Wheeler <Web-Slinger@outlook.com>
2020-07-28 10:14:20 +0000

    Added totalResults variable and comments

--- assets/js/completed.js
+++ assets/js/completed.js
@@ -1,13 +1,14 @@
-/*--I've created four variables. These variables will be used to help us display a user's score on the completed.html page,
+/*--I've created a handful of variables. These variables will be used to help us display a user's score on the completed.html page,
 they'll also allow us to enable our saveScore button when a name is typed into the Enter Name field, since it's default setting
-is set to disabled. This has been disabled so that users cannot save a score without entering a name first. The final variable
+is set to disabled. This has been disabled so that users cannot save a score without entering a name first. The next variable
 here pulls data from our localStorage. A user's score is saved into local storage so that we can display it on our completed.html
-page--*/
+page. The totalResults variable will ultimately only allow 10 results to be displayed on our leaderboard--*/
 
 const name = document.getElementById("name");
 const saveScore = document.getElementById("saveScore");
 const endScore = document.getElementById("endScore");
 const newScore = localStorage.getItem("newScore");
+const totalResults = 10;
 
 /*--The code below allows us to take the score we accumulated over the course of the quiz and display it on the completed.html
 page with a little added text for a more user friendly experience--*/
