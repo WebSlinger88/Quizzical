1529bc70eb4d1939c9359d118e321b32b7f3ef55
Lewis Wheeler <Web-Slinger@outlook.com>
2020-07-28 10:32:59 +0000

    Created results variable which will pull data from local storage. This data will be JSON.parse to convert it from a string to an object

--- assets/js/completed.js
+++ assets/js/completed.js
@@ -4,11 +4,17 @@ is set to disabled. This has been disabled so that users cannot save a score wit
 here pulls data from our localStorage. A user's score is saved into local storage so that we can display it on our completed.html
 page. The totalResults variable will ultimately only allow 10 results to be displayed on our leaderboard--*/
 
+/*--When we receive data from a web server the data is always a string, in this instance I don't want the data to be a string,
+I want the data to be an object so that I can display the data in that object on the leaderboard page. If I didn't have anything
+saved in local storage and I tried to run the application the console would flag as null. So the code below will try and pull
+whatever is in local storage OR it will give us an empty array. At no point do I want the console to return as null--*/
+
 const name = document.getElementById("name");
 const saveScore = document.getElementById("saveScore");
 const endScore = document.getElementById("endScore");
 const newScore = localStorage.getItem("newScore");
 const totalResults = 10;
+const results = JSON.parse(localStorage.getItem("results")) || [];
 
 /*--The code below allows us to take the score we accumulated over the course of the quiz and display it on the completed.html
 page with a little added text for a more user friendly experience--*/
