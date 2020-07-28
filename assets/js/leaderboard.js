089a709b1cff007a3e3e87178b8b1b1a55311a02
Lewis Wheeler <Web-Slinger@outlook.com>
2020-07-28 14:39:24 +0000

    Comments added for leaderboard.js variables

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,2 +1,7 @@
+/*--Two variables added for our leaderboard page. lbList is our variable that shall call our id within the leaderboard.html
+file. This will ultimately define the list of names and scores. The leaderboard variable is fetching data from local storage
+and converting it from a string to an object using JSON.parse as we have done previously in this project. ((See completed.js
+comments for information about this))--*/
+
 const lbList = document.getElementById("lb-list");
 const leaderboard = JSON.parse(localStorage.getItem("results")) || [];
