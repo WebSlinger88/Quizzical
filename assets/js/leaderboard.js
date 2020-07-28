eaecea5076937650093c3ba9434dfab68ccf4fb4
Lewis Wheeler <Web-Slinger@outlook.com>
2020-07-28 15:05:34 +0000

    Created .map function for score that shall create <li> within <ul> that'll display names and scores - Added comments for this

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -5,3 +5,15 @@ comments for information about this))--*/
 
 const lbList = document.getElementById("lb-list");
 const leaderboard = JSON.parse(localStorage.getItem("results")) || [];
+
+lbList.innerHTML = leaderboard 
+
+/*--The function below uses the map method to iterate through each score and for each one of those scores we want to add an <li>
+to our <ul> which is in leaderboard.html. As you can see below a list of names shall be created and a list of scores shall be 
+created. Map is basically taking an incoming array which in this instance is score, and it converts each item within it into a
+new array which is the string that is returned, as you can see below. Both name and score have their own classes defined and 
+they'll be styled separately within style.css--*/
+
+  .map(function(score){ 
+    return `<li class="lb-name">${score.name}</li>` + `<li class="lb-score">${score.score}</li>`; 
+  }) 
